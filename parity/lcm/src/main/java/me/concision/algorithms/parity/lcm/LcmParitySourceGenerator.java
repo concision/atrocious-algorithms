522c41175663ee6202cd101400dfa1ecf7640954
Concision <contact@concision.me>
2020-08-15 03:32:37 -0700

    feat(parity/lcm): Boilerplate source code generation

--- parity/lcm/src/main/java/me/concision/algorithms/parity/lcm/LcmParitySourceGenerator.java
+++ parity/lcm/src/main/java/me/concision/algorithms/parity/lcm/LcmParitySourceGenerator.java
@@ -1,33 +1,57 @@
 package me.concision.algorithms.parity.lcm;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.nio.file.Paths;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 public class LcmParitySourceGenerator {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = Paths.get(LcmParitySourceGenerator.class.getPackage().getName().replace('.', File.separatorChar), "Parity.java").toFile();
-        file.getParentFile().mkdirs();
+    /**
+     * Generated Parity.java location
+     */
+    private static final File PARITY_JAVA = Paths.get(
+            LcmParitySourceGenerator.class.getPackage().getName().replace('.', File.separatorChar),
+            "Parity.java"
+    ).toFile();
 
-        log.info("Generating Parity.java source file: {}", file.getAbsolutePath());
-        try (PrintStream stream = new PrintStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-            stream.println("package " + LcmParitySourceGenerator.class.getPackage().getName() + ";");
-            stream.println();
-            stream.println("public class Parity {");
-            stream.println("    public static boolean isEven(int n) {");
-            stream.println("        return n % 2 == 0;");
-            stream.println("    }");
-            stream.println();
-            stream.println("    public static boolean isOdd(int n) {");
-            stream.println("        return !isEven(n);");
-            stream.println("    }");
-            stream.println("}");
-        }
+    /**
+     * Initiate source code generation
+     *
+     * @param args an empty {@link String[]}
+     */
+    @SneakyThrows({InterruptedException.class})
+    public static void main(String[] args) {
+        log.info("Parity.java generated source file: {}", PARITY_JAVA.getAbsolutePath());
+
+        // create parent directories of Parity.java
+        PARITY_JAVA.getParentFile().mkdirs();
+
+        // determine processor parallelism; attempt to leave 1 processor available for system stability
+        int processors = Math.max(Runtime.getRuntime().availableProcessors() - 1, 1);
+        // submit task to a ForkJoinPool to cap a parallel Stream's computational usage
+        ForkJoinPool pool = new ForkJoinPool(processors);
+        pool.submit(() -> {
+            try {
+                generateSourceFile();
+            } catch (Throwable throwable) {
+                log.error("An unexpected exception occurred during execution", throwable);
+                System.exit(-1);
+            }
+        });
+
+        // shutdown pool and wait for computation to complete
+        pool.shutdown();
+        pool.awaitTermination(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Generate {@link #PARITY_JAVA} source file
+     */
+    private static void generateSourceFile() {
+
     }
 }
